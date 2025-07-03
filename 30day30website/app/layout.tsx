95d2a08c5b667bee29f8df198c57523f3923ad58
Abhishek Gurjar <120670964+abhishekboadgurjar@users.noreply.github.com>
2025-07-03 23:21:15 +0530

    Update layout.tsx

--- 30day30website/app/layout.tsx
+++ 30day30website/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: '30 Day 30 Website ',
+  description: 'Created with Abhishek Gurjar',
   generator: 'v0.dev',
 }
 
